af67e6a7712f2f0a3e97ff1474900ad212d7b393
valdir-alves3000 <valdiralves3000@gmail.com>
2023-10-21 10:15:18 -0300

    feat: Add Product and ProductImage components for displaying product information and images

---
+++ src/app/components/ProductImage.tsx
@@ -0,0 +1,34 @@
+'use client'
+import { ProductType } from "@/types/ProductType";
+import Image from 'next/image';
+import { useState } from 'react';
+
+type ProductImageProps = {
+  product: ProductType;
+  fill?: boolean
+}
+
+export function ProductImage({product,fill}:ProductImageProps) {
+  const [loading, setLoading] = useState(true)
+
+  return (
+    fill ? (
+      <Image 
+        src={product.image}
+        fill
+        alt={product.title}
+        className={`object-cover ${loading ? 'scale-110 blur-3xl grayscale' : 'scale-100 blur-0 grayscale-0'}`}
+        onLoadingComplete={()=>setLoading(false)}
+      />
+    ) : (
+      <Image 
+        src={product.image}
+        width={400}
+        height={700}
+        alt={product.title}
+        className={`object-cover ${loading ? 'scale-110 blur-3xl grayscale' : 'scale-100 blur-0 grayscale-0'}`}
+        onLoadingComplete={()=>setLoading(false)}
+      />
+    )
+  )
+}
