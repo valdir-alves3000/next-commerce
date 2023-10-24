2e589d0aee6f45bf699964f1eab2c9591ce3ba27
valdir-alves3000 <valdiralves3000@gmail.com>
2023-10-24 09:27:22 -0300

    feat(env): Add environment variables to the NodeJS.ProcessEnv namespace

---
+++ typings.d.ts
@@ -0,0 +1,15 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_NODE_ENV: "development" | "production"
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string
+    CLERK_SECRET_KEY: string
+    CLERK_WEBHOOK_SECRET: string
+    POSTGRES_URL: string
+    POSTGRES_PRISMA_URL: string
+    POSTGRES_URL_NON_POOLING: string
+    POSTGRES_USER: string
+    POSTGRES_HOST: string
+    POSTGRES_PASSWORD: string
+    POSTGRES_DATABASE: string
+  }
+}
