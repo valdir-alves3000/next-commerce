f813b3c87484d4002bd5cc02949b55b891bab54f
valdir-alves3000 <valdiralves3000@gmail.com>
2023-10-25 09:30:43 -0300

    feat(cart-drawer): add shopping cart drawer component

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -1,29 +1,31 @@
 "use client";
 import { useCartStore } from "@/store";
+import { CartDrawer } from "./CartDrawer";
 
 export function Cart() {
   const useStore = useCartStore();
   return (
-    <div
-      onClick={() => useStore.toggleCart()}
-      className="flex items-center cursor-pointer relative"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-5 w-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
+    <>
+      <div
+        onClick={() => useStore.toggleCart()}
+        className="flex items-center cursor-pointer relative"
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-        />
-      </svg>
-      <span
-        className="
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+          />
+        </svg>
+        <span
+          className="
             bg-teal-600 
             text-sm 
             font-bold 
@@ -33,9 +35,11 @@ export function Cart() {
             absolute
             left-3
             bottom-3"
-      >
-        {useStore.cart?.length}
-      </span>
-    </div>
+        >
+          {useStore.cart?.length}
+        </span>
+      </div>
+      {!useStore.isOpen && <CartDrawer />}
+    </>
   );
 }
